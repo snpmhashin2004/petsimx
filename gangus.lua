2e1277d1bec6539f370992a9bcc6d4c7ea78c09f
MatProjects <96875756+MatProjects@users.noreply.github.com>
2021-12-30 15:05:28 +0100

    Delete gangus.lua

--- gangus.lua
+++
@@ -1,8 +0,0 @@
-local playercoin = 99999999999999999
-local pet = "rainbowhugecat"
-
-function HatchEgg(Pet)
-  if playercoin = 99999999999999999
-    theme = dark
-    local vac = 1 
-    if vac = 2 give player santapaws
